16837c0a218a87207ff49f6754b949e744218471
maurolnl <mauroleoquiroga@gmail.com>
2022-07-06 11:10:51 -0300

    add carrito item component

---
+++ src/components/Carrito/CarritoItemComponent.tsx
@@ -0,0 +1,20 @@
+interface Props{
+  id: number;
+  nombre: string;
+  imagen: string;
+  onClick: (id: number) => void;
+}
+
+const CarritoItemComponent: React.FC<Props> = ({id, nombre, imagen, onClick}) => {
+  return (
+    <div key={id} className={`flex flex-row  border-b border-stone-400 py-2 px-3 items-center justify-between`}>
+    <div className="p-1 rounded-full bg-stone-400">
+      <img alt={`imagen-${nombre}`} src={imagen} />
+    </div>
+    <span className="text-white font-bold">{nombre}</span>
+    <button className=" text-stone-400 hover:text-stone-500 h-fit" onClick={() => onClick(id)}>✕</button> 
+  </div>
+  )
+}
+
+export default CarritoItemComponent
