75bb89a50f56f541496788a1784042fc21b8192f
maurolnl <mauroleoquiroga@gmail.com>
2022-07-06 11:24:14 -0300

    Update lista productos style

--- src/components/Productos/ListadoProductosComponent.tsx
+++ src/components/Productos/ListadoProductosComponent.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export const ListadoProductosComponent:React.FC<Props> = ({productos}) => {
   return (
-    <div className="grid grid-cols-2 gap-4">
+    <div className="grid grid-cols-1 justify-items-center gap-4 sm:grid-cols-2">
       {productos.map(producto => (
         <ProductoComponent key={producto.id} producto={producto} />
       ))}
