0eab5bd6c7bab5000186d0cd9015ef9558133bbc
maurolnl <mauroleoquiroga@gmail.com>
2022-07-05 21:18:48 -0300

    add producto component

---
+++ src/components/Productos/ProductoComponent.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+interface Props {
+  producto: IProductos;
+}
+
+const ProductoComponent:React.FC<Props> = ({producto}) => {
+  return (
+    <article className="bg-stone-700 py-5 px-5 flex max-w-fit flex-col gap-2 rounded-md border border-transparent hover:border-violet-600 hover:ease-in duration-150">
+      <div className="w-full flex justify-center">
+        <img alt="imagen-posión" className=" h-28 w-28" src={producto.imagen}/>
+      </div>
+      <span className="text-white text-base font-bold">{producto.nombre}</span>
+      <span className="text-base font-bold text-stone-400">{producto.descripcion}</span>
+      <button className="w-full rounded-2xl bg-violet-600 h-10 text-white font-bold text-base hover:bg-violet-500 hover:ease-in duration-100">Agregar</button>
+    </article>
+  )
+}
+
+export default ProductoComponent
