a6fa64a463784dbdc55d92b084f69ab17c0686a0
maurolnl <mauroleoquiroga@gmail.com>
2022-07-06 09:16:30 -0300

    Update producto style

--- src/components/Productos/ProductoComponent.tsx
+++ src/components/Productos/ProductoComponent.tsx
@@ -6,7 +6,12 @@ interface Props {
 
 const ProductoComponent:React.FC<Props> = ({producto}) => {
   return (
-    <article className="bg-stone-700 py-5 px-5 flex max-w-fit flex-col gap-2 rounded-md border border-transparent hover:border-violet-600 hover:ease-in duration-150">
+    <article className="bg-stone-700 py-5 px-5 flex w-64 sm:w-full relative flex-col gap-2 rounded-md border border-transparent hover:border-violet-600 hover:ease-in duration-150">
+      <div className='absolute px-2 bg-green-500 rounded-lg right-3 top-3'>
+        <span>
+          1 Gema
+        </span>
+      </div>
       <div className="w-full flex justify-center">
         <img alt="imagen-posión" className=" h-28 w-28" src={producto.imagen}/>
       </div>
