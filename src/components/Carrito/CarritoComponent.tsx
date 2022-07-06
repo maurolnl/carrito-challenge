9d2f560fe8911bfa79e8bc76f6b226e85237b28d
maurolnl <mauroleoquiroga@gmail.com>
2022-07-06 14:17:22 -0300

    Update carrito
    now carrito handles purchase and shows context data

--- src/components/Carrito/CarritoComponent.tsx
+++ src/components/Carrito/CarritoComponent.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import api from "../../api/api";
+import CarritoItemComponent from "./CarritoItemComponent";
 import { useCarrito } from "./context";
 
 interface Props {
@@ -5,29 +8,66 @@ interface Props {
 }
 
 export const CarritoComponent: React.FC<Props> = ({onClick}) => {
-  const { carrito, quantity } = useCarrito();
+  const { carrito, removeProduct, resetCarrito } = useCarrito();
+  const [success, setSuccess] = useState<boolean>(false);
+  const [error, setError] = useState<boolean>(false);
   
   const isCarritoEmpty = carrito.length === 0;
+
+  const formatCarrito = () => {
+    return carrito.map(producto => producto.id)
+  }
+
+  const handleBuy = async () => {
+    const idProductos = formatCarrito();
+    
+    try {
+      const response = await api.postOrder(idProductos);
+      console.log(response);
+      
+      if (response) {
+        setSuccess(true);
+        resetCarrito();
+
+        return;
+      }
+      setError(true);
+    } catch (error) {
+      setError(true);
+    }
+
+  }
 
   return (
     <div className="flex flex-col gap-7">
       <button className="rounded-2xl bg-violet-600 h-10 text-white font-bold text-base hover:bg-violet-500 hover:ease-in duration-100 px-3 w-fit" onClick={onClick}>Volver</button>
-      <div className="flex flex-col bg-stone-700">
-        <div className={`flex flex-row  border-b border-stone-400 py-2 px-3 items-center ${isCarritoEmpty ? "justify-center" : "justify-between"}`}>
-          {!isCarritoEmpty ? ( 
-            <>
-              <div className="p-1 rounded-full bg-stone-400">
-                <img alt="imagen-poción" src="http://localhost:3000/Icon2.png" />
-              </div>
-              <span className="text-white font-bold">HP 500</span>
-              <button className="px-2 h-fit" onClick={() => {}}>x</button> 
-            </>
+      <div className={`flex flex-col ${error || success ? "bg-transparent" : "bg-stone-700"}`}>
+        {success || error ? (
+          success ? (
+            <span className="text-white font-bold justify-center">Compra Realizada!</span>
           ) : (
-            <span className="text-white font-bold justify-center">No hay productos en su carrito.</span>
-          )}
-        </div>
+            <span className="text-white font-bold justify-center">Ha ocurrido un error en la compra.</span>
+          )
+        ) : (
+          isCarritoEmpty ? (
+            <div className={`flex flex-row  border-b border-stone-400 py-2 px-3 items-center "justify-center"`}>
+              <span className="text-white font-bold justify-center">No hay productos en su carrito.</span>
+            </div>
+          ):(
+            carrito.map(producto => {
+              return (
+                  <CarritoItemComponent key={producto.id} id={producto.id} nombre={producto.nombre} imagen={producto.imagen} onClick={(id) => removeProduct(id)} />
+                )
+            })
+          )
+        )}
       </div>
-      <button className="rounded-2xl bg-violet-600 h-10 text-white font-bold text-base hover:bg-violet-500 hover:ease-in duration-100 w-full" onClick={onClick}>Comprar</button>
+      <button 
+        className="rounded-2xl bg-violet-600 h-10 text-white font-bold text-base hover:bg-violet-500 hover:ease-in duration-100 w-full"
+        onClick={handleBuy}
+      >
+        Comprar
+        </button>
     </div>
   );
 };
