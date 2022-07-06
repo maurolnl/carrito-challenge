81689dc99dc880ebbe127b6eb608c3d504123251
maurolnl <mauroleoquiroga@gmail.com>
2022-07-06 14:02:01 -0300

    Update header style

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -8,13 +8,13 @@ export const HeaderComponent: React.FC<Props> = ({onClick}) => {
   const {gemas, quantity} = useCarrito();
   
   return (
-    <div className="bg-stone-700 py-4 px-8 flex justify-between items-center sticky top-0 shadow-md z-10">
-      <h1 className="text-white text-2xl font-bold">🧙‍♂️ Potion Shop</h1>
-      <div className="flex gap-2 items-center">
+    <div className="bg-stone-700 py-4 px-4 sm:px-8 flex justify-between items-center sticky top-0 shadow-md z-10">
+      <h1 className="text-white text-xl sm:text-2xl flex items-center flex-col sm:flex-row font-bold"><span>🧙‍♂️</span> Potion Shop</h1>
+      <div className="flex gap-2 flex-col sm:flex-row items-center">
         <img alt="gema icon" src="./gem.png" />
         <span>{gemas} Gemas</span>
       </div>
-      <button className="text-white hover:underline" onClick={onClick}>Ver Carrito ({quantity})</button>
+      <button className="text-white hover:underline text-sm sm:text-base" onClick={onClick}>Ver Carrito ({quantity})</button>
     </div>
   );
 };
