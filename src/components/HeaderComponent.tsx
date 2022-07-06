ed7ce78e5074abbb2032d424a2e90ad825e6647d
maurolnl <mauroleoquiroga@gmail.com>
2022-07-06 12:16:00 -0300

    Update HeaderComponent.tsx

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -11,7 +11,7 @@ export const HeaderComponent: React.FC<Props> = ({onClick}) => {
     <div className="bg-stone-700 py-4 px-8 flex justify-between items-center sticky top-0 shadow-md z-10">
       <h1 className="text-white text-2xl font-bold">🧙‍♂️ Potion Shop</h1>
       <div className="flex gap-2 items-center">
-        <img src="./gem.png" />
+        <img alt="gema icon" src="./gem.png" />
         <span>{gemas} Gemas</span>
       </div>
       <button className="text-white hover:underline" onClick={onClick}>Ver Carrito ({quantity})</button>
